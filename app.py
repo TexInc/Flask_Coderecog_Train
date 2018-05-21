a6aed4ac2e44c69295b223ccc4caf70247eda896
susmote <admin@susmote.com>
2018-05-21 20:29:17 +0800

    主要功能已完成

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+from predict_func import verify
+import os
+import pickle
+
+app = Flask(__name__)
+
+class ReviewForm(Form):
+    right_code_view = TextAreaField('', [validators.DataRequired(), validators.length(max=4)])
+
+
+@app.route('/')
+def index():
+    right_code = ''.join(list(verify('http://125.221.35.100/CheckCode.aspx', 'model/SVC_Model_zf.pkl')))
+    print(right_code)
+    form = ReviewForm(request.form)
+    return render_template('index.html', right_code=right_code)
+
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = ReviewForm(request.form)
+    if request.method == 'POST' and form.validate():
+        review = request.form['right_code_view']
+        return render_template('')
+
+
+
+if __name__ == '__main__':
+    app.run()
